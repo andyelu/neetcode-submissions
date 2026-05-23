f62c56cc2b8e72b55cb8ea4ce5fcb15781daba22
andyelu <126619706+andyelu@users.noreply.github.com>
2026-05-22 21:13:41 -0700

    Add: search-for-word-ii - submission-4

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-4.py
@@ -0,0 +1,66 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def add_word(self, word):
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+
+        curr.is_end = True
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        trie = Trie()
+        for w in words:
+            trie.add_word(w)
+
+        res = set()
+        visited_path = set()
+
+        m = len(board)
+        n = len(board[0])
+
+        def dfs(root, i, j, curr_word):
+            c = board[i][j]
+
+            if c not in root.children:
+                return
+            
+            child = root.children[c]
+            curr_word += c
+            visited_path.add((i,j))
+            
+            if child.is_end:
+                res.add(curr_word)
+                child.is_end = False
+
+            for nei in ((0,1), (1,0), (-1,0), (0,-1)):
+                ni = nei[0] + i
+                nj = nei[1] + j
+
+                if ((ni,nj) not in visited_path and 
+                    0 <= ni < m and 
+                    0 <= nj < n):
+                    dfs(child, ni, nj, curr_word)
+            visited_path.remove((i,j))
+
+            # if the child node is not a word ending and has no children
+            # it has no purpose and can be removed to clean up dead trie branch
+            if not child.children:
+                root.children.pop(c)
+
+        for i in range(m):
+            for j in range(n):
+                dfs(trie.root, i, j, "")
+
+        return list(res)
+            
